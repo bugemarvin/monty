acb9bd44ab749a6df84c97f88e1f8ceba3caa468
Marvin Kurland <bugemarvin@outlook.com>
2022-07-12 18:22:47 +0300

    initial main file

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  * main - entry to check execution for stacks.
+  * @argc: As the argumental count.
+  * @argv: As the argumental vector/value.
+  *
+  * Return: Always 0 on succes.
+  */
+
+int main(int argc, char **argv)
+{
+	return (0);
+}
